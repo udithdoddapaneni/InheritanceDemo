c502bacaf0681b8d01591d51593065af049b1bad
udithdoddapaneni <udithdoddapaneni@gmail.com>
2025-08-27 15:46:08 +0530

    Added more tests

--- UnitTests/InheritanceTests.cs
+++ UnitTests/InheritanceTests.cs
@@ -10,27 +10,82 @@ namespace UnitTests;
 public sealed class InheritanceTests
 {
     [TestMethod]
-    public void TestCarInheritance()
+    public void Test1()
     {
         Vehicle toyota = new Car("Toyota");
         Assert.AreEqual(toyota.Start(), "Staring the engine of the car named Toyota");
+    }
+    [TestMethod]
+    public void Test2()
+    {
         Vehicle audi = new Car("Audi");
         Assert.AreEqual(audi.Start(), "Staring the engine of the car named Audi");
     }
     [TestMethod]
-    public void TestAeroplaneInhertiance()
+    public void Test3()
     {
-        Vehicle aircraft1 = new Aeroplane();
-        Assert.AreEqual(aircraft1.Start(), "Taking off");
-        FlyingVehicle aircraft2 = new Aeroplane();
-        Assert.AreEqual(aircraft2.Start(), "Taking off");
+        Vehicle aircraft = new Aeroplane();
+        Assert.AreEqual(aircraft.Start(), "Taking off");
     }
     [TestMethod]
-    public void TestHelicopterInhertiance()
+    public void Test4()
     {
-        Vehicle heli1 = new Helicopter();
-        Assert.AreEqual(heli1.Start(), "Lifting off");
-        FlyingVehicle heli2 = new Helicopter();
-        Assert.AreEqual(heli2.Start(), "Lifting off");
+        FlyingVehicle aircraft = new Aeroplane();
+        Assert.AreEqual(aircraft.Start(), "Taking off");
+    }
+    [TestMethod]
+    public void Test5()
+    {
+        Vehicle heli = new Helicopter();
+        Assert.AreEqual(heli.Start(), "Lifting off");
+    }
+    [TestMethod]
+    public void Test6()
+    {
+        FlyingVehicle heli = new Helicopter();
+        Assert.AreEqual(heli.Start(), "Lifting off");
+
+    }
+    [TestMethod]
+    public void Test7()
+    {
+        Car toyota = new Car("Toyota");
+        Assert.AreEqual(toyota.Start(), "Staring the engine of the car named Toyota");
+    }
+    [TestMethod]
+    public void Test8()
+    {
+        Aeroplane aircraft = new Aeroplane();
+        Assert.AreEqual(aircraft.Start(), "Taking off");
+    }
+    [TestMethod]
+    public void Test9()
+    {
+        Helicopter heli = new Helicopter();
+        Assert.AreEqual(heli.Start(), "Lifting off");
+    }
+    [TestMethod]
+    public void Test10()
+    {
+        FlyingVehicle heli = new Helicopter();
+        Assert.AreEqual(heli.Fly(), "Lifting off");
+    }
+    [TestMethod]
+    public void Test11()
+    {
+        FlyingVehicle aircraft = new Aeroplane();
+        Assert.AreEqual(aircraft.Fly(), "Taking off");
+    }
+    [TestMethod]
+    public void Test12()
+    {
+        Aeroplane aircraft = new Aeroplane();
+        Assert.AreEqual(aircraft.Fly(), "Taking off");
+    }
+    [TestMethod]
+    public void Test13()
+    {
+        Car audi = new Car("Audi");
+        Assert.AreEqual(audi.GetCarName(), "Audi");
     }
 }
